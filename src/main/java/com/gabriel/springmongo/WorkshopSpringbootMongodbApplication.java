2f228bb04ff49ce194f4533b9ed0cbd4bad0a404
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-09 20:12:21 -0300

    primeiro commit

---
+++ src/main/java/com/gabriel/springmongo/WorkshopSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/springmongo/WorkshopSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
