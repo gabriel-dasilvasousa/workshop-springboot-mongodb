55a9dd2d3f8d48f545f04eace7758ec99bd605f5
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-10 21:22:37 -0300

    Obtendo um usuário por id

--- src/main/java/com/gabriel/springmongo/services/UserService.java
+++ src/main/java/com/gabriel/springmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.gabriel.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.springmongo.domain.User;
 import com.gabriel.springmongo.repository.UserRepository;
+import com.gabriel.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
 }
