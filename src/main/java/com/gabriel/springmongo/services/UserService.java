b6f393f106346ac1945a539e77c72f53390896da
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-11 11:56:54 -0300

    Atualização de usuário com PUT

--- src/main/java/com/gabriel/springmongo/services/UserService.java
+++ src/main/java/com/gabriel/springmongo/services/UserService.java
@@ -13,29 +13,41 @@ import com.gabriel.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
-		//Chama o método para verificar se o objeto existe, senão o próprio já lança uma exceção
-		findById(id); 
+		// Chama o método para verificar se o objeto existe, senão o próprio já lança
+		// uma exceção
+		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		Optional<User> userObj = repo.findById(obj.getId());
+		updateData(userObj.get(), obj);
+		return repo.save(userObj.get());
+	}
+
+	private void updateData(User userObj, User obj) {
+		userObj.setName(obj.getName());
+		userObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDto(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
