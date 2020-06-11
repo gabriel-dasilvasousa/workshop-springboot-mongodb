85ebeeacaa7386182437d6c6b29cad6fa58e95c7
gabriel-dasilvasousa <g.silva13455@gmail.com>
2020-06-10 22:04:33 -0300

    Deleção de Usuário com DELETE

--- src/main/java/com/gabriel/springmongo/resources/UserResource.java
+++ src/main/java/com/gabriel/springmongo/resources/UserResource.java
@@ -1,14 +1,19 @@
 package com.gabriel.springmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabriel.springmongo.domain.User;
 import com.gabriel.springmongo.dto.UserDTO;
@@ -34,4 +39,23 @@ public class UserResource {
 		UserDTO userDto = new UserDTO(user);
 		return ResponseEntity.ok().body(userDto);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User user = service.fromDto(objDto);
+		service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/com/gabriel/springmongo/services/UserService.java
+++ src/main/java/com/gabriel/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.springmongo.domain.User;
+import com.gabriel.springmongo.dto.UserDTO;
 import com.gabriel.springmongo.repository.UserRepository;
 import com.gabriel.springmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,19 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		//Chama o método para verificar se o objeto existe, senão o próprio já lança uma exceção
+		findById(id); 
+		repo.deleteById(id);
+	}
+	
+	public User fromDto(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
